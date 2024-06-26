605e341b0e808383a92aba3d2efa593fd50ab124
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-06-27 01:01:53 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,8 +14,8 @@ function App() {
             path={"/"}
             element={
               <div className="h-full w-full flex md:flex-row flex-col">
-                <a href={"/signup"} className="flex justify-center items-center md:w-1/2 w-full md:h-full h-1/2 hover:bg-gray-700 hover:text-white text-7xl font-bold">Sign up </a>
-                <a href={"/signin"} className="flex justify-center items-center md:w-1/2 w-full md:h-full h-1/2 hover:bg-gray-700 hover:text-white text-7xl font-bold">Sign in </a>
+                <a href={"/signup"} className="flex justify-center items-center md:w-1/2 w-full md:h-full h-1/2 hover:bg-gray-700 hover:text-white text-7xl font-light">Sign up </a>
+                <a href={"/signin"} className="flex justify-center items-center md:w-1/2 w-full md:h-full h-1/2 hover:bg-gray-700 hover:text-white text-7xl font-light">Sign in </a>
               </div>
             }
           ></Route>
